defd6cc63dd27cd97115ba5ca314bd226eb534b0
the-sink <lucianedean@gmail.com>
2021-03-17 12:46:58 -0700

    Update functions.js

--- modules/functions.js
+++ modules/functions.js
@@ -100,7 +100,7 @@ module.exports = (client) => {
     text = text
       .replace(/`/g, "`" + String.fromCharCode(8203))
       .replace(/@/g, "@" + String.fromCharCode(8203))
-      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+      .split(client.token).join("mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
 
     return text;
   };
